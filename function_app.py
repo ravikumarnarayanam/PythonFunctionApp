855f246abc4e8e361d17c3f16d8e65244df18d07
ravikumar.narayanam@gmail.com <ravikumar.narayanam@gmail.com>
2023-11-21 14:11:47 +0530

    changed to http trigger

--- function_app.py
+++ function_app.py
@@ -1,12 +1,9 @@
-import logging
 import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
-def timer_trigger(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function executed.')
+@app.function_name(name="HttpTrigger1")
+@app.route(route="req")
+def main(req: func.HttpRequest) -> str:
+    user = req.params.get("user")
+    return f"Hello, {user}!"
